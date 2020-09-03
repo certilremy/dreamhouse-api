c54440fb51e6d85e7a88da43ce7902fb2bdcf9de
Certil Remy <certilremy@gmail.com>
2020-09-02 20:35:07 -0400

    add validation for user an houses

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,5 @@
 class House < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true
+  validates :name, presence: true, length: { minimum: 8 }
+  validates :description, presence: true, length: { minimum: 15 }
   validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.username = username.downcase }
+  validates :username, presence: true, length: { minimum: 4 },
+                       uniqueness: true
 end
