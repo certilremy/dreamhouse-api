6fa49b6088ce73625007363059e6b3510e268393
Certil Remy <certilremy@gmail.com>
2020-09-03 11:21:36 -0400

    create favorites model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :houses 
+      resources :favorites
       post "/login", to: "users#login"
       post "/signup", to: "users#create"
       get "/auto_login", to: "users#auto_login"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_001501) do
+ActiveRecord::Schema.define(version: 2020_09_03_151010) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "houses", force: :cascade do |t|
     t.string "name"
