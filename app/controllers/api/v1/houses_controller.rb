86b5e2c0e5a0b848ed8fe318eac220e4acef7ae8
Certil Remy <certilremy@gmail.com>
2020-09-02 21:20:02 -0400

    add crud action for house

---
+++ app/controllers/api/v1/houses_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::HousesController < ApplicationController
+  before_action :set_house, only: %i[show edit update destroy]
+  def index
+    @houses = House.all
+    render json: { houses: @houses }
+  end
+
+  def show
+    render json: { house: @house }
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      render json: { house: @house }
+    else
+      render json: { error: 'Error saving the house' }
+    end
+  end
+
+  def update
+    if @house.update(house_params)
+      render json: { house: @house }
+    else
+      render json: { error: 'Error updating the house' }
+    end
+  end
+
+  def destroy
+    @house.destroy
+    render json: { 'message' => 'House was successfully deleted' }.to_json
+  end
+
+  private
+
+  def set_house
+    @house = House.find(params[:id])
+  end
+
+  def house_params
+    params.require(:house).permit(:name, :description, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resource :houses 
+      resources :houses 
       post "/login", to: "users#login"
       post "/signup", to: "users#create"
       get "/auto_login", to: "users#auto_login"
