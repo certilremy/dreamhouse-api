7770653d122b8b8a2b7df65b74742c99e17502d6
Certil Remy <certilremy@gmail.com>
2020-09-03 23:55:40 -0400

    fixed user test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'User API', type: :request do
   let!(:user) { create(:user) }
   let(:user_params) { { username: user.username }.to_json }
 
-  describe 'GET /api/v1/signup' do
+  describe 'POST /api/v1/signup' do
     before { post '/api/v1/signup', params: user_params }
 
     it 'returns status code 200' do
@@ -12,7 +12,7 @@ RSpec.describe 'User API', type: :request do
     end
   end
 
-  describe 'GET /api/v1/login' do
+  describe 'POST /api/v1/login ' do
     before { post '/api/v1/login', params: user_params }
 
     it 'returns status code 200' do
