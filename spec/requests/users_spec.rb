769e99dc87e5a75cb4de3a3ff0448e8ca91ea2d0
Certil Remy <certilremy@gmail.com>
2020-09-05 02:09:48 -0400

    fixed auth test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'User API', type: :request do
-  let!(:user) { create(:user) }
-  let(:user_params) { { username: user.username }.to_json }
+  let(:attributes) { { "username": "certilremytes" } }
 
   describe 'POST /api/v1/signup' do
-    before { post '/api/v1/signup', params: user_params }
+    before { post '/api/v1/signup', params: attributes }
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
+    it 'creates a new user' do
+      expect(json['token']).not_to be_empty
     end
   end
 
   describe 'POST /api/v1/login ' do
-    before { post '/api/v1/login', params: user_params }
+    before { post '/api/v1/login', params: attributes }
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
